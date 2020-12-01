9f7246e6103a61819e67f06933dc108dc3a2ceac
reesegrayallen <56320935+reesegrayallen@users.noreply.github.com>
2020-12-01 14:51:22 -0600

    Create ListNode.cpp

---
+++ lab02-linked-lists/prelab-LinkedList/ListNode.cpp
@@ -0,0 +1,19 @@
+/*
+Reese Allen (rga2uz)
+ListNode.cpp
+09-14-20
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+#include "List.h"
+#include "ListItr.h"
+
+ListNode::ListNode(){
+	value = 0;
+	next = NULL;
+	previous = NULL;
+}
